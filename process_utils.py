875cf2887957a6d478644d1d61da31079c910a94
shibeta <joshuagnar@gmail.com>
2025-09-14 11:18:03 +0800

    重写获取窗口的方法

--- process_utils.py
+++ process_utils.py
@@ -86,6 +86,59 @@ def get_window_info(window_name: str) -> Optional[Tuple[int, int]]:
         return None
 
 
+def find_window(window_title: str, process_name: str) -> Optional[Tuple[int, int]]:
+    """
+    一个更加可靠(?)的窗口查找器，用于找到一个标题和进程名称均匹配的，非0*0的窗口。
+
+    Args:
+        window_title: 目标窗口的标题。
+        process_name: 目标窗口的进程名称。
+
+    Returns:
+        一个包含 (hwnd, pid) 的元组，如果未找到窗口则返回 None。
+    """
+    # 找到所有的窗口
+    top_windows = []
+    win32gui.EnumWindows(lambda hwnd, top_windows: top_windows.append(hwnd), top_windows)
+
+    candidate_windows = []
+    for hwnd in top_windows:
+        # 检查标题是否匹配
+        if window_title == win32gui.GetWindowText(hwnd):
+            # 检查进程名称是否匹配
+            _, pid = win32process.GetWindowThreadProcessId(hwnd)
+            try:
+                process = psutil.Process(pid)
+                if process_name == process.name():
+                    candidate_windows.append(hwnd)
+            except (psutil.NoSuchProcess, psutil.AccessDenied):
+                continue
+
+    # 从候选者中找到最佳窗口
+    for hwnd in candidate_windows:
+        # 优先找非最小化的窗口
+        if not win32gui.IsIconic(hwnd):
+            # 检查窗口是否可见
+            if win32gui.IsWindowVisible(hwnd):
+                rect = win32gui.GetWindowRect(hwnd)
+                width = rect[2] - rect[0]
+                height = rect[3] - rect[1]
+                # 检查窗口是否具有尺寸
+                if width > 1 and height > 1:
+                    # 最佳选项
+                    _, pid = win32process.GetWindowThreadProcessId(hwnd)
+                    return hwnd, pid
+
+    # 如果没找到正在显示的，再检查是否有最小化的候选者
+    for hwnd in candidate_windows:
+        if win32gui.IsIconic(hwnd):
+            _, pid = win32process.GetWindowThreadProcessId(hwnd)
+            return hwnd, pid
+
+    # 遍历完所有窗口都没找到
+    return None, None
+
+
 def get_process_pid_by_window_handler(handler: int) -> int:
     """
     根据窗口句柄获取进程pid
@@ -162,7 +215,9 @@ def set_active_window(hwnd: int):
         try:
             if hwnd != win32gui.GetForegroundWindow():
                 win32gui.SetForegroundWindow(hwnd)
-                win32gui.ShowWindow(hwnd, SW_RESTORE)
+            win32gui.ShowWindow(hwnd, SW_RESTORE)
+            # 给 Windows 一点时间完成窗口重绘
+            time.sleep(0.5)
         except Exception as e:
             GLogger.error(f"将窗口({hwnd})设置为活动窗口时出错: {e}")
             return
