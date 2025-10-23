24469a5d72dd6dcf8d6d33c5253871374c0f763f
shibeta <joshuagnar@gmail.com>
2025-10-23 14:41:52 +0800

    优化?

--- gta_automator/game_process.py
+++ gta_automator/game_process.py
@@ -4,7 +4,9 @@ from typing import Optional
 from logger import get_logger
 from windows_utils import (
     find_window,
+    get_process_name,
     get_window_title,
+    close_window,
     kill_processes,
     resume_process_from_suspend,
     suspend_process_for_duration,
@@ -16,8 +18,8 @@ logger = get_logger(name="game_process")
 class GameProcess:
     """封装与游戏窗口和进程相关的各种方法"""
 
-    # 与 GTA V 相关的进程名称列表
-    GTA_PROCESS_NAMES = [
+    # 与 GTA V 增强版相关的进程名称列表
+    GTA_ASSOCIATED_PROCESS_NAMES = [
         "GTA5.exe",
         "GTA5_Enhanced.exe",
         "GTA5_Enhanced_BE.exe",
@@ -27,6 +29,10 @@ class GameProcess:
         "SocialClubHelper.exe",
         "Launcher.exe",
     ]
+    # GTA V 增强版进程名
+    GTA_PROCESS_NAMES = "GTA5_Enhanced.exe"
+    # GTA V 增强版窗口标题
+    GTA_WINDOW_TITLE = "Grand Theft Auto V"
 
     def __init__(
         self,
@@ -37,29 +43,34 @@ class GameProcess:
         self.pid = pid  # 进程ID
 
         # 注册退出处理函数，以确保Python程序退出时 GTA V 进程不会被挂起
-        atexit.register(self.resume_gta_process)
+        atexit.register(self.resume)
 
-    def update_gta_window_info(self):
+    def update_info(self, hwnd: Optional[int] = None, pid: Optional[int] = None):
         """
-        根据窗口标题和进程名称，更新 GTA V 窗口句柄和进程 PID。
+        传入窗口句柄和 PID ，更新对象的信息。
 
-        如果未找到 GTA V 窗口，将设置窗口句柄和 PID 为 None。
+        如果未提供，则根据窗口标题和进程名寻找窗口句柄和 PID 。未找到将设置窗口句柄和 PID 为 None。
         """
         # 仅适用于增强版
-        logger.info("正在更新 GTA V 窗口信息...")
-        window_info = find_window("Grand Theft Auto V", "GTA5_Enhanced.exe")
-        if window_info:
-            logger.debug(f"找到 GTA V 窗口。窗口句柄: {self.hwnd}, 进程ID: {self.pid}")
-            self.hwnd, self.pid = window_info
-            logger.info("更新 GTA V 窗口信息完成。")
+        logger.info("正在更新 GTA V 进程信息...")
+        if hwnd and pid:
+            logger.debug(f"使用传入的窗口句柄: {self.hwnd}, 进程ID: {self.pid} 更新进程信息。")
+            self.hwnd, self.pid = hwnd, pid
         else:
-            logger.error("未找到 GTA V 窗口，更新窗口信息失败。")
-            self.hwnd, self.pid = None, None
-            return
+            info = find_window(self.GTA_WINDOW_TITLE, self.GTA_PROCESS_NAMES)
+            if info:
+                logger.debug(f"找到 GTA V 窗口。窗口句柄: {self.hwnd}, 进程ID: {self.pid}")
+                self.hwnd, self.pid = info
+            else:
+                logger.error("未找到 GTA V 窗口。")
+                self.hwnd, self.pid = None, None
+                return
 
-    def suspend_gta_process(self, suspend_time: float):
+        logger.info("更新 GTA V 进程信息完成。")
+
+    def suspend(self, suspend_time: float):
         """
-        将 GTA V 进程挂起指定时间。
+        将 GTA V 进程挂起指定时间。如果 GTA V 未启动，则不做任何事。
 
         :param suspend_time: 挂起时间，单位秒
         """
@@ -68,12 +79,12 @@ class GameProcess:
                 suspend_process_for_duration(self.pid, suspend_time)
             except ValueError:
                 logger.error(f"挂起 GTA V 进程失败，GTA V 进程 PID({self.pid}) 无效。")
-                self.update_gta_window_info()
+                self.update_info()
             except Exception as e:
                 logger.error(f"挂起 GTA V 进程时，发生异常: {e}")
 
-    def resume_gta_process(self):
-        """将 GTA V 进程从挂起中恢复"""
+    def resume(self):
+        """将 GTA V 进程从挂起中恢复。如果 GTA V 未启动，则不做任何事。"""
         if self.pid:
             try:
                 resume_process_from_suspend(self.pid)
@@ -81,10 +92,10 @@ class GameProcess:
                 # 所有异常都不做处理
                 logger.error(f"恢复 GTA V 进程时，发生异常: {e}")
 
-    def kill_gta_process(self):
+    def kill(self):
         """杀死 GTA V 进程，并且清除窗口句柄和 PID 。"""
         logger.info("动作: 正在杀死 GTA V 相关进程...")
-        kill_processes(self.GTA_PROCESS_NAMES)
+        kill_processes(self.GTA_ASSOCIATED_PROCESS_NAMES)
         self.hwnd, self.pid = None, None
         logger.info("杀死 GTA V 相关进程完成。")
 
@@ -101,19 +112,28 @@ class GameProcess:
                 raise Exception("触发 alt+f4 退出失败") from e
 
     # --- 状态检查方法 ---
-    def is_game_started(self) -> bool:
-        """检查游戏是否启动。"""
-        window_info = find_window("Grand Theft Auto V", "GTA5_Enhanced.exe")
+    @staticmethod
+    def is_game_started() -> bool:
+        """
+        检查游戏是否启动。
+        **注意这个方法不会检查或修改对象记录的游戏信息，需要手动更新**。
+        """
+        window_info = find_window(GameProcess.GTA_WINDOW_TITLE, GameProcess.GTA_PROCESS_NAMES)
         if window_info:
-            logger.debug(f"GTA V 已启动。窗口句柄: {window_info[0]}, 进程ID: {window_info[1]}")
             return True
         else:
-            logger.debug("未找到 GTA V 窗口。GTA V 未启动。")
             return False
 
     def is_hwnd_valid(self) -> bool:
         """检查窗口句柄是否有效。"""
-        if self.hwnd and "Grand Theft Auto V" == get_window_title(self.hwnd):
+        if self.hwnd and self.GTA_WINDOW_TITLE == get_window_title(self.hwnd):
+            return True
+        else:
+            return False
+
+    def is_pid_vaild(self) -> bool:
+        """检查进程 PID 是否有效。"""
+        if self.pid and self.GTA_PROCESS_NAMES == get_process_name(self.pid):
             return True
         else:
             return False
