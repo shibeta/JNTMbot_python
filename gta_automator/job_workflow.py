07175eaad52952d99d0d79fae91b3c9795ea60ab
shibeta <joshuagnar@gmail.com>
2025-11-27 15:46:16 +0800

    更新: 移除面板消失后的卡单

--- gta_automator/job_workflow.py
+++ gta_automator/job_workflow.py
@@ -340,7 +340,7 @@ class JobWorkflow(_BaseWorkflow):
 
     def handle_post_job_start(self):
         """
-        处理差事启动后的一系列操作：等待加载、卡单、等待落地、再次卡单。
+        处理差事启动后的一系列操作：等待加载，等待落地，卡单。
 
         :raises ``OperationTimeout(OperationTimeoutContext.JOB_SETUP_PANEL_DISAPPEAR)``: 启动差事时，等待差事面板消失超时
         :raises ``OperationTimeout(OperationTimeoutContext.CHARACTER_LAND)``: 启动差事后，等待人物落地超时
@@ -353,9 +353,9 @@ class JobWorkflow(_BaseWorkflow):
             raise OperationTimeout(OperationTimeoutContext.JOB_SETUP_PANEL_DISAPPEAR)
 
         # 首次卡单
-        logger.info(f"面板已消失。{self.config.delaySuspendTime} 秒后将卡单。")
-        time.sleep(self.config.delaySuspendTime)
-        self.glitch_single_player_session()
+        # logger.info(f"面板已消失。{self.config.delaySuspendTime} 秒后将卡单。")
+        # time.sleep(self.config.delaySuspendTime)
+        # self.glitch_single_player_session()
 
         # 等待人物落地
         logger.info("差事加载完成！等待人物落地...")
